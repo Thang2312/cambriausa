5e06308eb7389b735f2b04e0788b9c92945321f1
Thang2312 <anhthangckb@gmail.com>
2018-08-13 18:49:11 +0700

    update header

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
-  let items = $('.main-nav__item');
-  items.each(function() {
-    $(this).removeClass('is-active');
-    $(this).hover(function() {
-      
-    })
-  })
+  let flyout;
+  // setup Trade
+
+  $('[tabindex=46] , ul.js-flyout a').hover(() => {
+    if($('[tabindex=46]').hasClass('is-active')) {
+      $('[tabindex=46]').removeClass('is-active');
+    } else {
+      $('[tabindex=46]').addClass('is-active');
+    }
+  });
+  $('.main-nav__item').hover(function(){
+    $('.main-nav__item').each((index, elem) => {
+      elem.classList.remove('is-active');
+    });
+    $(this).addClass('is-active');
+    flyout = $(this).data("flyout");
+    $('.flyout__inner.js-flyout .flyout__links').each((index, elem) => {
+      elem.style.display = 'none';
+    });
+    $(`.flyout__inner.js-flyout[data-flyout='${flyout}'] .flyout__links`).css('display','block');
+    $('.flyout-container.js-flyout-container').css('display','block');
+  });
+  $('.flyout-container.js-flyout-container').css('display','none');
 });
